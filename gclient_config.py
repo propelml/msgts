709d634887804ec8a62585a54ef6260519e1d2a5
Ryan Dahl <ry@tinyclouds.org>
2018-05-31 04:31:15 +0200

    Use protobuf

--- gclient_config.py
+++ gclient_config.py
@@ -31,9 +31,28 @@ GCLIENT_SOLUTION = [
             "build_for_node": True,
         },
     },
+    {
+        "name": "third_party/protobuf",
+        "url": "https://github.com/ry/protobuf_chromium.git",
+        "deps_file": "DEPS",
+    },
+    {
+        "name": "tools/protoc_wrapper",
+        "url": "https://chromium.googlesource.com/chromium/src/tools/protoc_wrapper",
+    },
+    {
+        "name": "third_party/zlib",
+        "url": "https://chromium.googlesource.com/chromium/src/third_party/zlib",
+    },
 ]
 
 spec = "solutions = %s" % GCLIENT_SOLUTION
 env = os.environ.copy()
 # gclient needs to have depot_tools in the PATH.
 subprocess.check_call(["gclient", "config", "--spec", spec], env=env)
+
+# Run sync if any of the dep dirs don't exist.
+
+# Run gn gen out/Default if out doesn't exist.
+
+# Run ninja -C out/Default 
